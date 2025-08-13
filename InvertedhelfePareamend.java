c2b5bf4928ef5b0fa5fd22f714a221fc08a9f841
Harsh Gupta <166977482+harsh-034@users.noreply.github.com>
2025-08-13 23:20:21 +0530

    Inverted Helfe Pareamend  loop partten question

---
+++ InvertedhelfePareamend.java
@@ -0,0 +1,16 @@
+public class InvertedhelfePareamend {
+    public static void main(String[] args) {
+        
+    
+     int n = 4;
+        for(int i=4 ; i n; i--)
+        {
+            for(int j=1; j>= i; j--)
+            {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+    
+}
