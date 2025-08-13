40c14b5e57e525d792b6e53f0b5ffe8558403064
Harsh Gupta <166977482+harsh-034@users.noreply.github.com>
2025-08-13 23:47:25 +0530

    Add question LInk of Leetcode

--- Power_of_Three.java
+++ Power_of_Three.java
@@ -1,5 +1,5 @@
 //Given an integer n, return true if it is a power of three. Otherwise, return false. An integer n is a power of three, if there exists an integer x such that n == 3x.
-
+// Leetcode link:    https://leetcode.com/problems/power-of-three?envType=daily-question&envId=2025-08-13
 
 /*
 Example 1:
