5330602ad67cc966569d03500eaa2499df5031f0
Harsh Gupta <166977482+harsh-034@users.noreply.github.com>
2025-08-20 23:28:26 +0530

    Add code and Output

--- Count_Square_Submatricesa_withAll_Ones.java
+++ Count_Square_Submatricesa_withAll_Ones.java
@@ -34,6 +34,41 @@ There is 1 square of side 2.
 Total number of   squares = 6 + 1 = 7.
 
   */
+class Solution {
+    public int countSquares(int[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int[][] num = new int[row][col];
+        int total = 0;
+
+        for(int i=0; i <row; i++)
+        {
+            for(int j=0; j< col; j++)
+            {
+                if(matrix[i][j]==1)
+                {
+                    if(i==0 || j==0)
+                    {
+                        num[i][j] = 1;
+                    }else{
+                        num[i][j]=1+ Math.min(num[i-1][j],Math.min(num[i][j-1],num[i-1][j-1]));
+                    }   
+                    total += num[i][j];
+                }
+            }
+        }
+         return total;
+    }
+        
+}
 
 
 
+/* OUTPUT:
+matrix = [[0,1,1,1],[1,1,1,1],[0,1,1,1]]
+Output 15
+Expected
+15
+
+
+*/
